ed2b5f7309089f9071f25a16a70fa9ee0b952519
alikemalcelik <alikemalcelik42@gmail.com>
2022-05-08 14:34:34 +0300

    8.05.2022

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ import googletrans
 import praw
 import os
 import dotenv
+import requests
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import cv2
+import pytesseract
+import unidecode
 
 dotenv.load_dotenv()
 
 openai.api_key = os.getenv("API_KEY")
+pytesseract.pytesseract.tesseract_cmd = os.getenv("TESSERACT_CMD")
 translator = googletrans.Translator()
 
 reddit = praw.Reddit(
@@ -19,6 +27,28 @@ reddit = praw.Reddit(
 
 def Translate(text, _from, to):
     return translator.translate(text, src=_from, dest=to).text
+
+def UrlToImg(url, save_as=''):
+    img = Image.open(BytesIO(requests.get(url).content))
+    if save_as:
+        img.save(save_as)
+    return np.array(img)
+
+def ResizeImg(img, max_height, max_width):
+    height, width = img.shape[:2]
+
+    if max_height < height or max_width < width:
+        scaling_factor = max_height / float(height)
+        if max_width/float(width) < scaling_factor:
+            scaling_factor = max_width / float(width)
+        img = cv2.resize(img, None, fx=scaling_factor, fy=scaling_factor, interpolation=cv2.INTER_AREA)
+    return img
+
+def ReadTexts(img):
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    threshold_img = cv2.threshold(gray_img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    text = pytesseract.image_to_string(threshold_img, lang='tur')
+    return text.strip()
 
 def Send(text):
     text = Translate(text, "tr", "en")
@@ -54,8 +84,13 @@ def CommentHotPosts(sbrddt, lmt=10):
 
         if not replied:
             text = f"{post.title} {post.selftext}"
+            if post.url.endswith(".jpg"):
+                img = UrlToImg(post.url)
+                img = ResizeImg(img, 400, 400)
+                img_text = unidecode.unidecode(ReadTexts(img)).strip()
+                text += img_text
             response = Send(text)
             post.reply(response)
 
-CommentHotPosts("ZargoryanGalaksisi")
+CommentHotPosts("ZargoryanGalaksisi", 10)
 ReplyAll()
