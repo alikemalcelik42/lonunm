caba69bb2b64fa29fe3e4dabd2e0eb9305339be1
alikemalcelik <alikemalcelik42@gmail.com>
2022-05-15 12:44:17 +0300

    15.05.2022

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from PIL import Image
 import cv2
 import pytesseract
 import unidecode
+from prawcore.exceptions import Forbidden
 import time
 
 dotenv.load_dotenv()
@@ -17,6 +18,10 @@ dotenv.load_dotenv()
 openai.api_key = os.getenv("API_KEY")
 pytesseract.pytesseract.tesseract_cmd = os.getenv("TESSERACT_CMD")
 translator = googletrans.Translator()
+
+subreddit_name = os.getenv("SUBREDDIT_NAME")
+limit = int(os.getenv("LIMIT"))
+countdown = int(os.getenv("COUNTDOWN"))
 
 reddit = praw.Reddit(
     client_id=os.getenv("CLIENT_ID"),
@@ -29,10 +34,8 @@ reddit = praw.Reddit(
 def Translate(text, _from, to):
     return translator.translate(text, src=_from, dest=to).text
 
-def UrlToImg(url, save_as=''):
+def UrlToImg(url):
     img = Image.open(BytesIO(requests.get(url).content))
-    if save_as:
-        img.save(save_as)
     return np.array(img)
 
 def ResizeImg(img, max_height, max_width):
@@ -49,13 +52,14 @@ def ReadTexts(img):
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     threshold_img = cv2.threshold(gray_img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
     text = pytesseract.image_to_string(threshold_img, lang='tur')
+    text = unidecode.unidecode(text)
     return text.strip()
 
-def Send(text):
+def SendAI(text):
     text = Translate(text, "tr", "en")
     response = openai.Completion.create(
         engine="text-davinci-002",
-        prompt=f"The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, funny and very friendly.\n\nHuman: {text}\nAI:",
+        prompt=f"The following is a conversation with an AI bot. The bot is helpful, creative, clever, funny and very friendly.\n\nHuman: {text}\nAI:",
         temperature=0.9,
         max_tokens=150,
         top_p=1,
@@ -71,8 +75,11 @@ def ReplyAll():
     inbox = reddit.inbox.unread()
 
     for message in inbox:
-        message.reply(Send(message.body))
-        message.mark_read()
+        try:
+            message.reply(SendAI(message.body))
+            message.mark_read()
+        except Forbidden:
+            message.mark_read()
 
 def CommentHotPosts(sbrddt, lmt=10):
     subreddit = reddit.subreddit(sbrddt)
@@ -88,10 +95,17 @@ def CommentHotPosts(sbrddt, lmt=10):
             if post.url.endswith(".jpg"):
                 img = UrlToImg(post.url)
                 img = ResizeImg(img, 400, 400)
-                img_text = unidecode.unidecode(ReadTexts(img)).strip()
+                img_text = ReadTexts(img)
                 text += img_text
-            response = Send(text)
-            post.reply(response)
+            response = SendAI(text)
+            
+            try:
+                post.reply(response)
+            except Forbidden:
+                pass
 
-ReplyAll()
-CommentHotPosts("ZargoryanGalaksisi", 10)
+time.sleep(countdown)
+while True:
+    ReplyAll()
+    CommentHotPosts(subreddit_name, limit)
+    time.sleep(countdown)
